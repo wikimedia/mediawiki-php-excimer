4cc649be12183c672867e9184d09d7cd9c604d91
Timo Tijhof <krinkle@fastmail.com>
2024-02-20 05:03:32 +0000

    Release 1.2.0
    Bug: T316540
    Bug: T351245
    Change-Id: I553b3a81afe3ded3c33a88be512cfa6ff3a8cff9

--- php_excimer.h
+++ php_excimer.h
@@ -19,7 +19,7 @@
 extern zend_module_entry excimer_module_entry;
 #define phpext_excimer_ptr &excimer_module_entry
 
-#define PHP_EXCIMER_VERSION "1.1.1"
+#define PHP_EXCIMER_VERSION "1.2.0"
 
 #ifdef PHP_WIN32
 #	define PHP_EXCIMER_API __declspec(dllexport)
